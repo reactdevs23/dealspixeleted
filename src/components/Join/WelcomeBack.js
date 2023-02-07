c22287838d14fd50b229c16a6304e3116b0f619e
dalimkumar604 <dalimkumar212582@gmail.com>
2023-02-07 16:58:35 +0600

    google del

--- src/components/Join/WelcomeBack.js
+++ src/components/Join/WelcomeBack.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { GoogleLogin } from "react-google-login";
+
 import { Link } from "react-router-dom";
 import CheckBox from "../Checkbox/CheckBox";
 
@@ -51,15 +51,9 @@ const WelcomeBack = () => {
 
         <button className={styles.button}>Sign in</button>
 
-        <GoogleLogin
-          className="google"
-          clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
-          buttonText="Sign in with Google"
-          onSuccess={responseGoogle}
-          onFailure={responseGoogle}
-          cookiePolicy={"single_host_origin"}
-          style={{ boxShadow: "none" }}
-        />
+        <a href="#/" className={styles.googleButton}>
+          FcGoogle Sign in with Google
+        </a>
         <p className={styles.haveAccount}>
           Sign up Not a member?
           <Link className={styles.signUp} to="/createaccount">
